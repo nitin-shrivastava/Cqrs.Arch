17424e022eefa330337c90e9a866ebccd674e6cf
Nitin Shrivastava <Nitin.Shrivastava@ey.com>
2021-01-27 23:35:28 +0530

    service and repository

---
+++ CqrsArch.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using CqrsArch.Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CqrsArch.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
---
+++ CqrsArch.Application/ViewModel/CourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CqrsArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CqrsArch.Application.ViewModel
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+           
+    }
+}
