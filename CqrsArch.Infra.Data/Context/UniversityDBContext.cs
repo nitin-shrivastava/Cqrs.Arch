33d9b08661c0736fcd894ddfd141c5ced7889e3a
Nitin Shrivastava <Nitin.Shrivastava@ey.com>
2021-01-27 23:21:39 +0530

    Inital commit for datalayer db context

---
+++ CqrsArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,19 @@
+﻿using CqrsArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CqrsArch.Infra.Data.Context
+{
+    public class UniversityDBContext:DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
