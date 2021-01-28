e2c64fe07720746866700cdd8fa12bdab0336db4
Nitin Shrivastava <Nitin.Shrivastava@ey.com>
2021-01-28 16:40:29 +0530

    Upgraded .net core 5.0 and added service, IOC
    Added IoC container

---
+++ CqrsArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CqrsArch.Application.Interfaces;
+using CqrsArch.Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CqrsArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
