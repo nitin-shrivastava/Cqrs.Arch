61a4ad52f900c05ffc35e89157d169c7a1d68697
Nitin Shrivastava <nitin.671@gmail.com>
2021-01-28 19:19:45 +0530

    Update IoC for mediator handler

--- CqrsArch.Infra.IoC/DependencyBootstraper.cs
+++ CqrsArch.Infra.IoC/DependencyBootstraper.cs
@@ -1,7 +1,13 @@
-﻿using CqrsArch.Application.Interfaces;
+﻿using Cqrs.Arch.Domain.Core.Bus;
+using Cqrs.Arch.Infra.ServiceBus;
+using CqrsArch.Application.Interfaces;
 using CqrsArch.Application.Services;
+using CqrsArch.Domain.CommandHandlers;
+using CqrsArch.Domain.Commands;
 using CqrsArch.Domain.Interfaces;
+using CqrsArch.Infra.Data.Context;
 using CqrsArch.Infra.Data.Repository;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -14,9 +20,13 @@ namespace CqrsArch.Infra.IoC
     public class DependencyBootstraper
     {
         public static void RegisterServices(IServiceCollection services)
-        {
+        {//Domain InMemory
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+            //Domain Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
             services.AddScoped<ICourseService, CourseService>();
             services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<UniversityDBContext>();
         }
     }
 }
